8c7d8c54ff4dbd49806c2ed7197b2af8acf3d6bc
Paulo Cândido <paulo.candido@g.globo>
2022-06-04 16:55:58 -0300

    add requestId to track when request was received and when it was processed

--- setups/queue/heavy-process/src/index.js
+++ setups/queue/heavy-process/src/index.js
@@ -4,10 +4,12 @@ import os from 'os'
 const queue = new Queue('calc-queue', 'redis://redis:6379')
 
 queue.process((job, done) => {
+  const { requestId } = job.data
+
   for (let i = 0; i < 1000000000; i++) {
     //simulate complex processing
   }
 
-  console.log(`processed by ${os.hostname()}`)
+  console.log(`processed ${requestId} by ${os.hostname()} at ${new Date().toISOString()}`)
   done()
 })
--- setups/queue/light-api/src/index.js
+++ setups/queue/light-api/src/index.js
@@ -1,14 +1,18 @@
 import Queue from 'bull'
 import express from 'express'
 import os from 'os'
+import { v4 as uuid } from 'uuid'
 
 const app = express()
 const queue = new Queue('calc-queue', 'redis://redis:6379')
 
 app.get('/', (req, res) => {
-  queue.add({ data: '' })
-  console.log(`queued by ${os.hostname()}`)
-  res.send(`queued by ${os.hostname()}`)
+  const requestId = uuid()
+  queue.add({ requestId })
+
+  const message = `queued ${requestId} by ${os.hostname()} at ${new Date().toISOString()}`
+  console.log(message)
+  res.send(message)
 })
 
 app.listen(8080, () => {
