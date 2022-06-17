c09100833d98ae4ae19fc5bdd6627e8c7cac0ac6
Paulo Cândido <paulo.candido@g.globo>
2022-06-16 21:43:15 -0300

    change heavy-process to write on mongo (io) instead of fake process

--- setups/queue/heavy-process/src/index.js
+++ setups/queue/heavy-process/src/index.js
@@ -1,16 +1,30 @@
+import { MongoClient } from 'mongodb'
 import Queue from 'bull'
 import os from 'os'
 
 const queue = new Queue('calc-queue', 'redis://redis:6379')
+let collection
+
+async function getCollection() {
+  if (!collection) {
+    const client = new MongoClient('mongodb://mongo:27017')
+    await client.connect()
+    const db = client.db('api')
+    collection = db.collection('requests')
+  }
+
+  return collection
+}
 
 queue.process((job, done) => {
-  const start = new Date().getTime()
+  const now = new Date().getTime()
   const { start: requestStart } = job.data
-  for (let i = 0; i < 1000000000; i++) {
-    //simulate complex processing
-  }
-  const end = new Date().getTime()
 
-  console.log(`processed by ${os.hostname()} in ${end - start}ms. total time = ${end - requestStart}ms`)
+  const coll = await getCollection()
+  coll.insertOne({
+    node: os.hostname(),
+    totalTime: now - requestStart
+  })
+
   done()
 })
